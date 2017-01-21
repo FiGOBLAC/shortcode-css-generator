09f6158e3ccccaab487571cb8d897abc4df3a81e
Julius Cirwithian <figoblacmedia@yahoo.com>
2017-01-20 19:56:43 -0500

    Corrected  name of called class

--- functions/functions.php
+++ functions/functions.php
@@ -4,7 +4,7 @@ require dirname( __FILE__ , 2 ) . '/class-shortcode-cssg.php';
 
 function shortcode_cssg( $shortcode, $defaults ){
 
-    $cssg = new Church_Core_Shortcode_CSSG();
+    $cssg = new Shortcode_CSSG();
 
     $cssg->shortcode_cssg( $shortcode, $defaults );
 
